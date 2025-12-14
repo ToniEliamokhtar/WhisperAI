4a1257f24d5d28e9d264262a70fef3dea2e1f30a
ToniEliamokhtar <2271763@cmontmorency.qc.ca>
2025-12-14 15:43:07 -0500

    [Add] ajout de Whisper au Backend

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import whisper
+import tempfile
+import os
 
 app = FastAPI()
+
+model = whisper.load_model("base")
 
 @app.get("/")
 def root():
     return {"message": "Backend Whisper OK"}
+
+@app.post("/transcribe")
+async def transcribe_audio(file: UploadFile = File(...)):
+    # On crée un fichier temporaire pour stocker l'audio uploadé
+    with tempfile.NamedTemporaryFile(delete=False) as temp_audio:
+        temp_audio.write(await file.read())
+        temp_path = temp_audio.name
+
+    # Transcription
+    result = model.transcribe(temp_path)
+
+    # Nettoyage pour la gestion de mémoire
+    os.remove(temp_path)
+
+    return {
+        "filename": file.filename,
+        "text": result["text"]
+    }
