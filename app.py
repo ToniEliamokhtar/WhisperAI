27637ff59d13b0cb72afb7ec7a5bc1dd6f70098c
ToniEliamokhtar <2271763@cmontmorency.qc.ca>
2025-12-14 16:21:45 -0500

    [Add] Ajout des autres fonctionnalités. Tout est testé et backend fonctionnel.
    Backend FastAPI finit

--- app.py
+++ app.py
@@ -13,18 +13,27 @@ def root():
 
 @app.post("/transcribe")
 async def transcribe_audio(file: UploadFile = File(...)):
-    # On crée un fichier temporaire pour stocker l'audio uploadé
-    with tempfile.NamedTemporaryFile(delete=False) as temp_audio:
+    # 1. Sauvegarder l'audio reçu dans un fichier temporaire
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
         temp_audio.write(await file.read())
         temp_path = temp_audio.name
 
-    # Transcription
-    result = model.transcribe(temp_path)
+    # 2. Transcription normale (langue d'origine)
+    result = model.transcribe(temp_path)  # ex: texte en français
+    original_text = result["text"]
+    detected_language = result["language"]
 
-    # Nettoyage pour la gestion de mémoire
+    # 3. Traduction en anglais
+    translated = model.transcribe(temp_path, task="translate")
+    translated_text = translated["text"]
+
+    # 4. Nettoyage
     os.remove(temp_path)
 
+    # 5. Réponse JSON
     return {
         "filename": file.filename,
-        "text": result["text"]
+        "language": detected_language,
+        "text_original": original_text,
+        "text_english": translated_text
     }
